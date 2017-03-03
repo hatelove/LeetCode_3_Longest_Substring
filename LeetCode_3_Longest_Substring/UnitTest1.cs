33ab9e22cf8cab3a9a6a0ecaf86884f077f3c70b
hatelove <jay.hatelove@gmail.com>
2017-03-03 17:30:02 +0800

    add a failed test case: s is "bb" should return 1

--- LeetCode_3_Longest_Substring/UnitTest1.cs
+++ LeetCode_3_Longest_Substring/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LeetCode_3_Longest_Substring
@@ -15,6 +16,12 @@ namespace LeetCode_3_Longest_Substring
         private static void AssertLength(string s, int expected)
         {
             Assert.AreEqual(expected, new Solution().LengthOfLongestSubstring(s));
+        }
+
+        [TestMethod]
+        public void s_is_bb_should_return_1()
+        {
+            AssertLength("bb", 1);
         }
     }
 

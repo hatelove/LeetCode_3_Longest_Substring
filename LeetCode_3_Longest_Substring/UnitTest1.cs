6a8fee02be69553f8c726ab6218b5dafb0071535
hatelove <jay.hatelove@gmail.com>
2017-03-03 17:23:59 +0800

    fix test case

--- LeetCode_3_Longest_Substring/UnitTest1.cs
+++ LeetCode_3_Longest_Substring/UnitTest1.cs
@@ -7,10 +7,10 @@ namespace LeetCode_3_Longest_Substring
     public class UnitTest1
     {
         [TestMethod]
-        public void s_is_a_should_return_a()
+        public void s_is_a_should_return_1()
         {
             var s = "a";
-            Assert.AreEqual("a", new Solution().LengthOfLongestSubstring(s));
+            Assert.AreEqual(1, new Solution().LengthOfLongestSubstring(s));
         }
     }
 

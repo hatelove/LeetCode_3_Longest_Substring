ab151c94af71ed1e297d224a2abd9ef7e3ea275d
hatelove <jay.hatelove@gmail.com>
2017-03-03 17:22:56 +0800

    add the first test case: s is "a" should return "a"

---
+++ LeetCode_3_Longest_Substring/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_3_Longest_Substring
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void s_is_a_should_return_a()
+        {
+            var s = "a";
+            Assert.AreEqual("a", new Solution().LengthOfLongestSubstring(s));
+        }
+    }
+
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
